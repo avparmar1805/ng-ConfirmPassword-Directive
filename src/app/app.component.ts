186d6943360b18ed161602d906494ff48b8a022e
Rahul Pandey <rahul.pandey@nagarro.com>
2017-07-06 15:30:21 +0530

    Directive to check confirm password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.interface';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    public user: User;
+
+    ngOnInit() {
+        // initialize model here
+        this.user = {
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    save(model: User, isValid: boolean) {
+        // call API to save customer
+        console.log(model, isValid);
+    }
 }
